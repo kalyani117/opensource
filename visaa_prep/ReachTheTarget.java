fec57de5efeeb591faafe03c50551de2f4b98bd5
kalyani117 <kalyanivadakapuram@gmail.com>
2024-11-17 22:14:27 +0530

    Create ReachTheTarget.java

---
+++ visaa_prep/ReachTheTarget.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            int t = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println(t-c+1);
+        }
+    }
+}
